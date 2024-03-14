c72c20a63a535f452d71b1d080f1a531498c8a41
frank-Regal <fregal@utexas.edu>
2024-03-14 14:13:22 -0500

    first push

---
+++ lang_sam/setup.py
@@ -0,0 +1,12 @@
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="lang_sam",
+    py_modules=["lang_sam", "utils"],
+    version="1.0.0",
+    description="",
+    author="",
+    packages=find_packages(),
+)
